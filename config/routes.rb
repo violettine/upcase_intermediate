8ec1286e888279eb9ee0d87bfb1b1251123b6a4d
violettine <violettine@freenet.de>
2015-05-19 15:35:36 +0200

    first init, forgot to add files first

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'homes#show', via: :get
+  resource :dashboard, only: :show
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :shouts, only: [:create, :show]
+end
