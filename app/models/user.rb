d83bffd06ea8ffd6b1c39c5e1c62672acb4734ac
violettine <violettine@freenet.de>
2015-05-20 12:56:22 +0200

    create following_relationship to get user.followed_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_many :shouts
+  # SELECT * FROM following_relationship WHERE follower_id = ?
+  has_many :following_relationship, foreign_key: :follower_id
+  # for :followed_user to work we need :following_relationship
+  #   to use the follower_id as key, because here follower(_id) is my 
+  #   user and he has followed_users whom he follows. With this is 
+  #   can use user.followed_users. 
+  has_many :followed_users, through: :following_relationship
 end
