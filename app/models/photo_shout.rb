7573ab547fe15144df0a5902ef34363ae729590a
violettine <violettine@freenet.de>
2015-05-20 09:48:00 +0200

    fix validation error of paperclip

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -2,4 +2,8 @@ class PhotoShout < ActiveRecord::Base
   has_attached_file :image, styles: {
     shout: "200x200>"
   }
+  # Validate content type
+  validates_attachment_content_type :image, :content_type => /\Aimage/
+  # Validate filename
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
 end
