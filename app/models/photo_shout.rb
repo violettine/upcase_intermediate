ae7a506c5d8d122ae30b1fdf680bcede73cf2891
violettine <violettine@freenet.de>
2015-05-20 09:50:26 +0200

    style fix with rubocop

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,9 +1,9 @@
 class PhotoShout < ActiveRecord::Base
   has_attached_file :image, styles: {
-    shout: "200x200>"
+    shout: '200x200>'
   }
   # Validate content type
-  validates_attachment_content_type :image, :content_type => /\Aimage/
+  validates_attachment_content_type :image, content_type: /\Aimage/
   # Validate filename
-  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
 end
